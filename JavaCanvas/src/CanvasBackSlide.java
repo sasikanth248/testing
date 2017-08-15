15b6f0b568d9821b6822055730318ceafff54ed4
sasikanth <sasikanth@krishtipati>
2017-08-15 07:54:08 +0530

    changes

--- JavaCanvas/src/CanvasBackSlide.java
+++ JavaCanvas/src/CanvasBackSlide.java
@@ -41,6 +41,7 @@ public class CanvasBackSlide extends Canvas {
 			}
 			g.setColor(Color.BLACK);
 		}
+		
 		else if(("horizontal stripes").equalsIgnoreCase(type)){
 			for(int j=0;j<401;j+=10){
 				g.drawRect(0, j, 400, 5);
